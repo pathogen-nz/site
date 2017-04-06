f601b23e13cb43d83084de270d56ed05f87d653f
Joost De Cock <joost@decock.org>
2017-04-06 15:07:05 +0000

    Added the freesewing header and some style improvements ported from hugo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,41 +1,33 @@
 import React, { Component, PropTypes } from "react"
 import Link from "react-router/lib/Link"
-import TitleStripe from "../TitleStripe"
 
 export default class Header extends Component {
   render() {
-    if (this.props.style === "blog") {
-      return (
-        <header className="page-header fade-oc">
-          <div id="top-logo">
-            <Link href="/" title="To the homepage">
-              <img src="/assets/img/logo/logo-black.svg" alt="The freesewing logo" />
-            </Link>
-          </div> 
-        </header>
-      )
-    }
-    else if (this.props.style === "spread") {
-      return (
-        <header className="page-header fade-oc">
-          <div id="home-logo">
-            <div id="home-logo-background" className="bg-thematic"></div>
-            <Link href="/" title="To the homepage">
-              <img src="/assets/img/logo/logo-cutout.svg" alt="The freesewing logo" />
-            </Link>
-          </div> 
-          <TitleStripe title={ this.props.title } />
-        </header>
-      )
-    }
     return (
-      <header className="page-header fade-oc">
-        <div id="top-logo">
-          <Link href="/" title="To the homepage">
-            <img src="/assets/img/logo/logo-black.svg" alt="The freesewing logo" />
-          </Link>
-        </div> 
-        <TitleStripe narrow title={ this.props.title } />
+      <header className="header" id="header">
+        <div className="skew">
+          <div className="header-inner">
+            <div id="top-logo">
+              <Link className="on-bg" to="/" title="Home" >
+                <img src="/assets/img/logo/logo-white.svg" alt="The freesewing logo" className="img img-responsive" />
+              </Link>
+            </div>
+            <div className="masthead">
+              <span id="masthead-main">
+                <Link className="on-bg" to="/" title="Home" >{"freesewing"}</Link>
+              </span>
+              <span id="masthead-sub">
+                <Link to="/" className="on-bg" title="Home" >{"an open source platform for made-to-measure sewing patterns"}</Link>
+              </span>
+              <ul className="mini-menu"> 
+                <li><Link to="/about"  className="on-bg" activeClassName="active">{"About"}</Link></li>
+                <li><Link to="/donate" className="on-bg" activeClassName="active">{"Donate"}</Link></li>
+                <li><Link to="/gitter" className="on-bg" activeClassName="active">{"Talk to us"}</Link></li>
+                <li><Link to="/blog/announcing-freesewing/" className="on-bg" activeClassName="active">{"Test"}</Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
       </header>
     )
   }
