fdd735b54c88d7840646f5468c21f1fe0d601795
Joost De Cock <joost@decock.org>
2017-04-10 12:14:46 +0000

    Rewrote BlogHeader as a stateless functional component

--- src/components/BlogHeader/index.js
+++ src/components/BlogHeader/index.js
@@ -1,38 +1,35 @@
-import React, { Component, PropTypes } from "react"
+import React, { PropTypes } from "react"
 import Link from "react-router/lib/Link"
 import dateFormat from "dateformat"
 
-export default class BlogHeader extends Component {
-  render() {
-      // eslint-disable-next-line no-console
-      console.log( dateFormat(this.props.head.date, 'ddd, dd mmm yyyy') )
-    return (
-      <div className="post fade-oc container">
-        <div className="row mt-5 fade-oc" id="posthead">
-          <div className="col-md-10 col-lg-10 offset-lg-1 offset-md-1 mt-5">
-            <ul className="post-meta" id="top">
-              <li><Link className="line" to={ this.props.head.authorlink }>{ this.props.head.author }</Link></li>
-              <li>{ dateFormat(this.props.head.date, 'dddd dd mmmm yyyy') }</li>
-              <li>{ this.props.head.category }</li>
-            </ul>
-            <h1 className="post">{ this.props.head.title }</h1>
-            <figure className="image">
-              <Link className="credit" to={ this.props.head.creditlink }>{ this.props.head.credit }</Link>
-              <Link className="hover-shadow" target="_BLANK" to={ this.props.head.image } title={ this.props.head.caption }>
-                <img alt={ this.props.head.caption } src={ this.props.head.image } className="img img-responsive" />
-              </Link>
-              <figcaption> 
-                <span className="caption">{ this.props.head.caption }</span>
-              </figcaption>
-            </figure>
-          </div>
+const BlogHeader = ({head}) => {
+  return (
+    <div className="post fade-oc container">
+      <div className="row mt-5 fade-oc" id="posthead">
+        <div className="col-md-10 col-lg-10 offset-lg-1 offset-md-1 mt-5">
+          <ul className="post-meta" id="top">
+            <li><Link className="line" to={ head.authorlink }>{ head.author }</Link></li>
+            <li>{ dateFormat(head.date, 'dddd dd mmmm yyyy') }</li>
+            <li>{ head.category }</li>
+          </ul>
+          <h1 className="post">{ head.title }</h1>
+          <figure className="image">
+            <Link className="credit" to={ head.creditlink }>{ head.credit }</Link>
+            <Link className="hover-shadow" target="_BLANK" to={ head.image } title={ head.caption }>
+              <img alt={ head.caption } src={ head.image } className="img img-responsive" />
+            </Link>
+            <figcaption> 
+              <span className="caption">{ head.caption }</span>
+            </figcaption>
+          </figure>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 BlogHeader.propTypes = {
   head : PropTypes.object.isRequired,
 }
 
+export default BlogHeader;
