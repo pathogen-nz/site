790f47a417be70b8ce694897a050fc69e574069a
Joost De Cock <joost@decock.org>
2017-04-10 17:40:12 +0000

    Rewrote MainMenu as a stateless functional component

--- src/components/dumb/MainMenu/index.js
+++ src/components/dumb/MainMenu/index.js
@@ -1,24 +1,25 @@
 import React, { Component } from "react"
 import { Link } from "phenomic"
+import Icon from "../Icon"
 
-export default class MainMenu extends Component {
-
-  render() {
-    return (
-            <div id="oc-left-wrapper">
-                <span className="oc-overlay"></span>
-                <a className="oc-toggle left burger" id="hamburger">
-                    <i className="icon-menu" aria-hidden="true" id="hamburger-icon"></i>
-                </a>
-                <div id="oc-left" className="oc-panel" data-side="left">
-                    <h4>{ "Documentation" }</h4>
-                    <ul>
-                        <li><Link href="/designer">{ "For designers" }</Link></li>
-                        <li><Link href="/developer">{ "For developers" }</Link></li>
-                        <li><Link href="/contribute">{ "For contributors" }</Link></li>
-                    </ul>
-                </div> 
-            </div> 
-        )
-  }
+const MainMenu = () => {
+    
+  return (
+    <div id="oc-left-wrapper">
+      <span className="oc-overlay"></span>
+      <Link to={"#"} className="oc-toggle left burger" id="hamburger">
+        <Icon name={"menu"} size={"38"}/>
+      </Link>
+      <div id="oc-left" className="oc-panel" data-side="left">
+        <h4>{ "Documentation" }</h4>
+        <ul>
+          <li><Link to="/designer">{ "For designers" }</Link></li>
+          <li><Link to="/developer">{ "For developers" }</Link></li>
+          <li><Link to="/contribute">{ "For contributors" }</Link></li>
+        </ul>
+      </div> 
+    </div> 
+  )
 }
+
+export default MainMenu
