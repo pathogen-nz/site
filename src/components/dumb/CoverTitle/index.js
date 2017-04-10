0c702c45409e9c626343d94e94e313bbb975c9a9
Joost De Cock <joost@decock.org>
2017-04-10 17:27:43 +0000

    Rewrote CoverTitle as a stateless functional component

--- src/components/dumb/CoverTitle/index.js
+++ src/components/dumb/CoverTitle/index.js
@@ -1,14 +1,14 @@
-import React, { Component, PropTypes } from "react"
+import React, { PropTypes } from "react"
 
-export default class CoverTitle extends Component {
-  render() {
-    return (
-      <h1 className="cover-heading" id="title">{ this.props.title }</h1>
-    )
-  }
+const CoverTitle = ({title}) => {
+  return (
+    <h1 className="cover-heading" id="title">{ title }</h1>
+  )
 }
 
 CoverTitle.propTypes = {
   title : PropTypes.string.isRequired,
 }
+
+export default CoverTitle;
 
